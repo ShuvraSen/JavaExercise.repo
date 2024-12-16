db9b573f8bf966b893d3a652be85063029c5a2a0
ShuvraSen <shuvra.sen.it@gmail.com>
2024-12-16 00:14:10 -0600

    increment with post and pre

---
+++ src/com/Excercises/IncrementConcept.java
@@ -0,0 +1,15 @@
+package com.Excercises;
+
+public class IncrementConcept {
+
+	public static void main(String[] args) {
+		int a=10;
+		System.out.println(a++);
+		System.out.println(a);
+		int b=11;
+		System.out.println(++b);
+		System.out.println(b);
+
+	}
+
+}
